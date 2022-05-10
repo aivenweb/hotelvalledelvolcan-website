0ba7a2c6c59ee03ec19b97215f517b060df9ec7e
naniitevez <nicole.tevez@fizzmod.com>
2022-05-09 23:43:00 -0300

    agrego event listener en el header para efecto de menu

--- src/components/home/header/Header.tsx
+++ src/components/home/header/Header.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeaderTop from "./HeaderTop";
 import MenuHeader from "./MenuHeader";
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
+  const [scrollOnTop, setScrollOnTop] = useState(true)
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, );
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    if (position < 2) {
+      setScrollOnTop(true)
+    } else setScrollOnTop(false);
+  };
+  // "header-sky header-top-sky"
   return (
-    <header className="header-sky header-top-sky">
+    <header className={scrollOnTop ? ("header-sky") : ("header-sky header-top-sky")}>
       <div className="container">
         <HeaderTop />
       </div>
