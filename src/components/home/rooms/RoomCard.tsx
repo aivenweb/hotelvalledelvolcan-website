cc76e4f5e7996e2175625dce298c52e8c80af5aa
naniitevez <nicole.tevez.97@gmail.com>
2022-05-22 04:00:01 -0300

    agrego las opciones de alojamiento

--- src/components/home/rooms/RoomCard.tsx
+++ src/components/home/rooms/RoomCard.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 
-interface RoomCardProps {}
+interface RoomCardProps {
+  image: string;
+  imageDescription: string;
+  title: string;
+  price: number;
+}
 
-const RoomCard: React.FC<RoomCardProps> = () => {
+const RoomCard: React.FC<RoomCardProps> = ({
+  image,
+  imageDescription,
+  title,
+  price,
+}) => {
   return (
-    <div></div>
+    <div className="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+      <div className="wrap-box">
+        <div className="box-img">
+          <img src={image} alt={imageDescription} className="img-responsive" />
+        </div>
+        <div className="rooms-content">
+          <h4 className="sky-h4">{title}</h4>
+          <p className="price">${price} / Per Night</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/home/rooms/Rooms.tsx
+++ src/components/home/rooms/Rooms.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import './Rooms.css'
+import RoomCard from "./RoomCard";
+import "./Rooms.css";
 
 interface RoomsProps {}
 
@@ -7,24 +8,31 @@ const Rooms: React.FC<RoomsProps> = () => {
   return (
     <section id="rooms">
       <div className="container">
-        <h2 className="title-room"> Nuestras Habitaciones</h2>
+        <h2 className="title-room"> Nuestros precios</h2>
         <p className="line"></p>
         <div className="wrap-rooms">
-            <div className="row">
-                <div className="rooms">
-                    <div className="col-lg-4 col-md-4 col-sm-6 col-xs-6">
-                        <div className="wrap-box">
-                            <div className="box-img">
-                                <img src="" alt="" className="img-responsive" />
-                            </div>
-                            <div className="rooms-content">
-                                <h4 className="sky-h4">Luxury Room</h4>
-                                <p className="price">$320 / Per Night</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+          <div className="row">
+            <div className="rooms">
+              <RoomCard
+                title="Jornada sin pensión"
+                image="/assets/images/habitacion2-hotel-valle-del-volcan.jpg"
+                imageDescription="alt"
+                price={500}
+              />
+              <RoomCard
+                title="Jornada con media pensión"
+                image="/assets/images/pool-hotel-valle-del-volcan.jpg"
+                imageDescription="alt"
+                price={600}
+              />
+              <RoomCard
+                title="Jornada con pensión completa"
+                image="/assets/images/vinos-hotel-valle-del-volcan.jpg"
+                imageDescription="alt"
+                price={700}
+              />
             </div>
+          </div>
         </div>
       </div>
     </section>
