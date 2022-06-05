8c64a2dc86a90cd67ea4df1e139ea4ae7f2ac721
naniitevez <nicole.tevez.97@gmail.com>
2022-06-05 01:47:50 -0300

    estilizo calendario

--- src/components/reservation/ReservationCalendar.tsx
+++ src/components/reservation/ReservationCalendar.tsx
@@ -7,16 +7,29 @@ interface ReservationCalendarProps {}
 const ReservationCalendar: React.FC<ReservationCalendarProps> = () => {
   const [date, setDate] = useState(new Date());
 
-  console.log(date);
-
   return (
     <div className=" col-lg-9 col-md-8 col-sm-12 col-xs-12">
-      <Calendar
-        onChange={setDate}
-        value={date}
-        selectRange={true}
-        minDate={new Date()}
-      />
+      <div className="reservation_content bg-gray">
+        <h2 className="reservation-heading">Disponibilidad</h2>
+        <div>
+          <div id="calendar-1" className="col-sm-6">
+            <Calendar
+              onChange={setDate}
+              value={date}
+              selectRange={true}
+              minDate={new Date()}
+            />
+          </div>
+          <div id="calendar-2" className="col-sm-6">
+            <Calendar
+              onChange={setDate}
+              value={date}
+              selectRange={true}
+              minDate={new Date()}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
