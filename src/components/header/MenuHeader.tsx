86d2e912697d506138b4894132fb5c04274bcc9d
naniitevez <nicole.tevez.97@gmail.com>
2022-08-05 00:19:13 -0300

    agrego logo del sitio

--- src/components/header/MenuHeader.tsx
+++ src/components/header/MenuHeader.tsx
@@ -25,7 +25,7 @@ const MenuHeader: React.FC<MenuHeaderProps> = () => {
               to="/"
               title="Skyline"
             >
-              <img src="./Home 1_files/sky-logo-header.png" alt="#" />
+              <img src="/assets/images/logo-hotel-valle-del-volcan.png" alt="Logo de Hotel Valle del Volcán" width="85px" />
             </Link>
           </div>
           <div className="collapse navbar-collapse">
--- src/components/home/footer/Footer.tsx
+++ src/components/home/footer/Footer.tsx
@@ -32,7 +32,7 @@ const Footer: React.FC<FooterProps> = () => {
               <div className="col-xs-12 col-sm-12 col-md-3 col-lg-3">
                 <div className="footer-logo text-center list-content">
                   <a href="/index.html" title="Skyline">
-                    <img src="./Home 1_files/sky-logo-footer.png" alt="" />
+                    <img src="/assets/images/logo-hotel-valle-del-volcan.png" alt="Logo de Hotel Valle del Volcán" width="130px" />
                   </a>
                 </div>
               </div>
